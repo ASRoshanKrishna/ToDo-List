dba1f857057bf44a2e3ebce3589f4db5d70f3209
Roshan Krishna A.S <asroshank@gmail.com>
2024-06-25 19:28:08 +0530

    add intro content

--- src/index.js
+++ src/index.js
@@ -5,7 +5,31 @@ import todos from './todos.js';
 import { format } from 'date-fns';
 import { PubSub } from './PubSub.js';
 
-let arr = ["projectOne", "one","first","2024-09-01", false, 'High'];
+let arr = ["Welcome",
+        "Welcome!",
+        "To get started try selecting the default todo list provided on the left. Also + button is present on the bottom that you can use to create your own todos!",
+        "2030-01-01",
+        false, 
+        "Low"];
+
 PubSub.Publish("creatingTodos", arr);
-let arr1 = ["projectOne", "two","two","2024-11-02", false, 'Low'];
+
+let arr1 = ["ToDo", 
+        "Complete your first task!",
+        "Once you've finished your first task you can click the checkbox next to the task to mark it as complete. A completed task will not be deleted unless you opt to.",
+        "2030-01-01",
+        false, 
+        "High"];
+
 PubSub.Publish("creatingTodos", arr1);
+
+let arr2 = ["ToDo",
+        "Editing tasks",
+        "You can also edit the todo using the edit icon next to it!",
+        "2030-01-01",
+        false,
+        "Medium"];
+
+PubSub.Publish("creatingTodos", arr2);
+
+PubSub.Publish("callPrintTodos", arr[0]);
